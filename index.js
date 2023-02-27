142450b54afabd762b004fe46e043b8376dbe1ac
Jessica Oboh <jessicarichardson@Jessicas-MBP.lan>
2023-02-26 22:46:14 -0700

    schema

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const express = require('express');
 const mysql = require('mysql2');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
-    // TODO: Add MySQL password here
-    password: '',
+    password: 'process.env.DB_PASSWORD',
     database: 'employees_db'
   },
   console.log(`Connected to the employees_db database.`)
